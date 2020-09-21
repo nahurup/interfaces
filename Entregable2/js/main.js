c6c37a411013393eaf41d9ba29ccda8317203de7
nahurup <nahuelruppel10@gmail.com>
2020-09-21 11:58:49 -0300

    2 canvas

--- Entregable2/js/main.js
+++ Entregable2/js/main.js
@@ -1,25 +1,5 @@
 let canvas = document.querySelector("#canvas");
 let ctx = canvas.getContext('2d');
+let grid = document.querySelector("#grid");
+let ctxGrid = grid.getContext('2d');
 
-let grid = [[],[],[],[],[],[],[],[]];
-let winner;
-
-function start(){
-    for (let i = 0; i < grid.length; i++) {
-        for (let j = 0; j < 8; j++) {
-            grid[i][j] = new Cell(canvas, ctx, 'base', i);
-        }
-    }
-    draw();
-}
-
-function draw(){
-    ctx.fillStyle = "rgb(54, 54, 54)";
-    ctx.fillRect(220,30,560,560);
-
-    for (let i = 0; i < grid.length; i++) {
-        for (let j = 0; j < 8; j++) {
-            grid[i][j].draw(ctx,(255)+(70*i),(590-35)-(70*j));
-        }
-    }
-}
